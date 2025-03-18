3289d7616275ea8f0a09aad6647c53e1ba27c319
usergolden25 <usergolden25@canvas.ed>
2025-03-17 19:48:09 -0700

    CRUD: all routes functional

--- mern-social/backend/src/controllers/user.controller.js
+++ mern-social/backend/src/controllers/user.controller.js
@@ -26,8 +26,56 @@ const list = async (req, res) => {
         })
     }
 }
+const userById = async (req, res, next, id) => {
+    try {
+        let user = await User.findById(id)
+        if (!user){
+            return res.status('400').json({
+                error: "User not found"
+            })
+        }
+        req.profile = user;
+        next()
+    } catch (err) {
+        return res.status('400').json({
+            error: "Could not retrieve user"
+        })
+    }
+};
+
+const update = async (req, res) => {
+    try {
+        let user = req.profile
+        user = extend(user, req.body)
+        user.updated = Date.now()
+        await user.save();
+        res.json(user)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+const remove = async (req, res) => {
+    try {
+        let user = req.profile
+        let deletedUser = await user.remove()
+        res.json(deletedUser)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+const read =  (req, res) => {
+    return res.json(req.profile)
+}
 
 export default {
     create,
     list,
+    update,
+    remove,
+    userById,
+    read,
 }
--- mern-social/backend/src/routes/user.routes.js
+++ mern-social/backend/src/routes/user.routes.js
@@ -7,11 +7,11 @@ router.route('/api/users')
     .get(userCtrl.list)
     .post(userCtrl.create)
 
-// router.route('/api/users/:userId')
-//     .get(userCtrl.read)
-//     .put(userCtrl.update)
-//     .delete(userCtrl.remove)
+router.route('/api/users/:userId')
+    .get(userCtrl.read)
+    .put(userCtrl.update)
+    .delete(userCtrl.remove)
 
-// router.param('userId', userCtrl.userById);
+router.param('userId', userCtrl.userById);
 
 export default router;
